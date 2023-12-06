3523065bf5e9ff15d5157935cbbb43319dc5525b
Rufino John <aguilarufino@gmail.com>
2023-12-06 14:19:29 +0800

    sample raw sql

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,6 +2,8 @@
 
 namespace App\Controllers;
 
+use CodeIgniter\Database\RawSql;
+
 class Home extends BaseController
 {
     public function index(): string
@@ -18,7 +20,14 @@ class Home extends BaseController
         // $query = $builder->select('id, first_name')->get();
         // SELECT id, first_name FROM authors;
 
-        $query = $builder->select('id,first_name,last_name, CONCAT(first_name, " ", last_name) as fullname')->get();
+        // $query = $builder->select('id,first_name,last_name, CONCAT(first_name, " ", last_name) as fullname')->get();
+        // $query = $builder->select('id,first_name,last_name, CONCAT(first_name, " ", last_name) as fullname')->where('id', 1)->get();
+
+        $sql = "CONCAT(first_name, ' ', last_name) as fullname";
+
+        $builder->select(new RawSql($sql));
+        $query = $builder->get();
+        // SELECT CONCAT(first_name, ' ', last_name) as fullname FROM authors;
 
 
 

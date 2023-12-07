86588962dd35ffd800999c206d4fba7e1a527437
Rufino John <aguilarufino@gmail.com>
2023-12-07 14:03:14 +0800

    added routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,6 +5,15 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
+$routes->setDefaultNamespace('App\Controllers');
+$routes->setDefaultController('Home');
+$routes->setDefaultMethod('index');
+$routes->setTranslateURIDashes(false);
+$routes->set404Override();
+
 $routes->get('/', 'Home::index');
+
+$routes->resource('offices', ['controller'=>'OfficeController', 'except'=>'new,edit']);
+$routes->resource('tickets', ['controller'=>'TicketController', 'except'=>'new,edit']);
 
 service('auth')->routes($routes);

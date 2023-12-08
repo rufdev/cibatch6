e8f80488b1a15365a87526d5157db48547fc629c
Rufino John <aguilarufino@gmail.com>
2023-12-08 16:46:10 +0800

    added dashboard data

--- app/Controllers/DashboardController.php
+++ app/Controllers/DashboardController.php
@@ -8,6 +8,29 @@ class DashboardController extends BaseController
 {
     public function index()
     {
-        return view('pages/dashboard');
+        $ticketModel = new \App\Models\Ticket();
+        $officeModel = new \App\Models\Office();
+        $data['totaltickets'] = $ticketModel->countAll();
+        $data['totalpending'] = $ticketModel->where('state','PENDING')->countAllResults();
+        $data['totalprocessing'] = $ticketModel->where('state','PROCESSING')->countAllResults();
+        $data['totalresolved'] = $ticketModel->where('state','RESOLVED')->countAllResults();
+
+        $data['totallow'] = $ticketModel->where('severity','LOW')->countAllResults();
+        $data['totalmedium'] = $ticketModel->where('severity','MEDIUM')->countAllResults();
+        $data['totalhigh'] = $ticketModel->where('severity','HIGH')->countAllResults();
+        $data['totalcritical'] = $ticketModel->where('severity','CRITICAL')->countAllResults();
+
+        $data['barchartdata1'] = $officeModel->select("offices.code AS office_name, COUNT(tickets.id) AS ticket_count")
+        ->join("tickets","tickets.office_id = offices.id")
+        ->groupBy("offices.id")
+        ->findAll();
+
+        $data['barchartdata2'] = $ticketModel->select("severity, COUNT(id) AS ticket_count")
+        ->groupBy("severity")
+        ->findAll();
+
+
+
+        return view('pages/dashboard',$data);
     }
 }
--- app/Views/pages/dashboard.php
+++ app/Views/pages/dashboard.php
@@ -1,5 +1,222 @@
 <?= $this->extend('templates/admin_template'); ?>
 
 <?= $this->section('contentarea'); ?>
-<h1>DASHBOARD PAGE</h1>
+<div class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+                <h1 class="m-0">Dashboard</h1>
+            </div><!-- /.col -->
+        </div><!-- /.row -->
+    </div><!-- /.container-fluid -->
+</div>
+<section class="content">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-3 col-6">
+                <!-- small box -->
+                <div class="small-box bg-info">
+                    <div class="inner">
+                        <h3><?= $totaltickets ?></h3>
+
+                        <p>Total Tickets</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-6">
+                <div class="small-box bg-danger">
+                    <div class="inner">
+                        <h3><?= $totalpending ?></h3>
+
+                        <p>Total Pending</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-6">
+                <div class="small-box bg-warning">
+                    <div class="inner">
+                        <h3><?= $totalprocessing ?></h3>
+
+                        <p>Total Processing</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-6">
+                <div class="small-box bg-success">
+                    <div class="inner">
+                        <h3><?= $totalresolved ?></h3>
+
+                        <p>Total Resolved</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-3 col-6">
+                <!-- small box -->
+                <div class="small-box bg-info">
+                    <div class="inner">
+                        <h3><?= $totallow ?></h3>
+
+                        <p>LOW</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-6">
+                <div class="small-box bg-orange">
+                    <div class="inner">
+                        <h3><?= $totalmedium ?></h3>
+
+                        <p>MEDIUM</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-6">
+                <div class="small-box bg-warning">
+                    <div class="inner">
+                        <h3><?= $totalhigh ?></h3>
+
+                        <p>HIGH</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-6">
+                <div class="small-box bg-danger">
+                    <div class="inner">
+                        <h3><?= $totalcritical ?></h3>
+
+                        <p>CRITICAL</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card card-success">
+                    <div class="card-header">
+                        <h3 class="card-title">TOTAL TICKET BY OFFICE</h3>
+
+                        <div class="card-tools">
+                            <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                                <i class="fas fa-minus"></i>
+                            </button>
+                            <button type="button" class="btn btn-tool" data-card-widget="remove">
+                                <i class="fas fa-times"></i>
+                            </button>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <div class="chart">
+                            <canvas id="barChart1" style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%;"></canvas>
+                        </div>
+                    </div>
+                    <!-- /.card-body -->
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card card-success">
+                    <div class="card-header">
+                        <h3 class="card-title">TOTAL TICKET BY SEVERITY</h3>
+
+                        <div class="card-tools">
+                            <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                                <i class="fas fa-minus"></i>
+                            </button>
+                            <button type="button" class="btn btn-tool" data-card-widget="remove">
+                                <i class="fas fa-times"></i>
+                            </button>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <div class="chart">
+                            <canvas id="barChart2" style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%;"></canvas>
+                        </div>
+                    </div>
+                    <!-- /.card-body -->
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<?= $this->endSection(); ?>
+
+<?= $this->section('pagescript'); ?>
+<script>
+    $(function() {
+        let barChartCanvas1 = $('#barChart1').get(0).getContext('2d');
+        let barChartCanvas2 = $('#barChart2').get(0).getContext('2d');
+
+        let barchartdata1 = <?= json_encode($barchartdata1) ?>;
+        let barchartdata2 = <?= json_encode($barchartdata2) ?>;
+
+        let barChartData1 = {
+            labels: barchartdata1.map(function(item) {
+                return item.office_name
+            }),
+            datasets: [{
+                label: 'Total Ticket',
+                backgroundColor: '#007bff',
+                borderColor: '#007bff',
+                borderWidth: 1,
+                data: barchartdata1.map(function(item) {
+                    return item.ticket_count
+                })
+            }]
+        };
+
+        let barChartData2 = {
+            labels: barchartdata2.map(function(item) {
+                return item.severity
+            }),
+            datasets: [{
+                label: 'Total Severity',
+                backgroundColor: '#007bff',
+                borderColor: '#007bff',
+                borderWidth: 1,
+                data: barchartdata2.map(function(item) {
+                    return item.ticket_count
+                })
+            }]
+        }
+
+        new Chart(barChartCanvas1, {
+            type: 'bar',
+            data: barChartData1,
+            option: {
+                responsive: true,
+                maintainAspectRatio: false,
+                datasetFill: false,
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true,
+                            stepSize: 1
+                        }
+                    }]
+                }
+            }
+        });
+
+        new Chart(barChartCanvas2, {
+            type: 'bar',
+            data: barChartData2,
+            option: {
+                responsive: true,
+                maintainAspectRatio: false,
+                datasetFill: false,
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true,
+                            stepSize: 1
+                        }
+                    }]
+                }
+            }
+        });
+
+    });
+</script>
 <?= $this->endSection(); ?>

e35069ba13decbe0987d59c569765f42aa38a4c9
Rufino John <aguilarufino@gmail.com>
2023-12-08 14:25:08 +0800

    added modal

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -18,7 +18,7 @@ $routes->get('dashboard', 'DashboardController::index');
 $routes->post('offices/list', 'OfficeController::list');
 $routes->post('tickets/list', 'TicketController::list');
 
-$routes->resource('offices', ['controller'=>'OfficeController', 'except'=>'new,edit', 'filter'=>'auth']);
+$routes->resource('offices', ['controller'=>'OfficeController', 'except'=>'new,edit']);
 $routes->resource('tickets', ['controller'=>'TicketController', 'except'=>'new,edit']);
 
 service('auth')->routes($routes);
--- app/Views/pages/offices.php
+++ app/Views/pages/offices.php
@@ -12,6 +12,11 @@
 </div>
 <section class="content">
     <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-md-12">
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modalID">Add Office</button>
+            </div>
+        </div>
         <div class="row">
             <div class="col-md-12">
                 <table id="dataTable" class="table table-bordered table-striped">
@@ -26,6 +31,27 @@
                     </thead>
                 </table>
             </div>
+        </div>
+        <div class="modal fade" id="modalID">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h4 class="modal-title">Office Details</h4>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <p>One fine body&hellip;</p>
+                    </div>
+                    <div class="modal-footer justify-content-between">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                </div>
+                <!-- /.modal-content -->
+            </div>
+            <!-- /.modal-dialog -->
         </div>
     </div>
 </section>

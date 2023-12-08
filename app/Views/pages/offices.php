047d9a2b539c76a171fca6a5978222ac87f13e77
Rufino John <aguilarufino@gmail.com>
2023-12-08 14:33:14 +0800

    added office form

--- app/Views/pages/offices.php
+++ app/Views/pages/offices.php
@@ -42,32 +42,39 @@
                         </button>
                     </div>
                     <div class="modal-body">
-                        <form>
+                        <form class="needs-validation" novalidate>
                             <div class="card-body">
                                 <div class="form-group">
-                                    <label for="exampleInputEmail1">Email address</label>
-                                    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
-                                </div>
-                                <div class="form-group">
-                                    <label for="exampleInputPassword1">Password</label>
-                                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-                                </div>
-                                <div class="form-group">
-                                    <label for="exampleInputFile">File input</label>
-                                    <div class="input-group">
-                                        <div class="custom-file">
-                                            <input type="file" class="custom-file-input" id="exampleInputFile">
-                                            <label class="custom-file-label" for="exampleInputFile">Choose file</label>
-                                        </div>
-                                        <div class="input-group-append">
-                                            <span class="input-group-text">Upload</span>
-                                        </div>
+                                    <label for="code">Code</label>
+                                    <input type="text" class="form-control" id="code" name="code" placeholder="Enter Office Code" required>
+                                    <div class="valid-feedback">
+                                        Looks good!
                                     </div>
+                                    <div class="invalid-feedback">
+                                        Please enter a valid office code.
+                                    </div>  
                                 </div>
-                                <div class="form-check">
-                                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                                <div class="form-group">
+                                    <label for="name">Name</label>
+                                    <input type="text" class="form-control" id="name" name="name" placeholder="Enter Office Name" required>
+                                    <div class="valid-feedback">
+                                        Looks good!
+                                    </div>
+                                    <div class="invalid-feedback">
+                                        Please enter a valid office name.
+                                    </div>  
                                 </div>
+                                <div class="form-group">
+                                    <label for="email">Email</label>
+                                    <input type="email" class="form-control" id="email" name="email" placeholder="Enter Office Email" required>
+                                    <div class="valid-feedback">
+                                        Looks good!
+                                    </div>
+                                    <div class="invalid-feedback">
+                                        Please enter a valid office email.
+                                    </div>  
+                                </div>
+                               
                             </div>
                             <!-- /.card-body -->
 
